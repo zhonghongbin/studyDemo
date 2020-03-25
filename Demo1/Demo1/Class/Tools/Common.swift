fbeba3f0588eeaf111f765bf6b400eee0e088dd3
zhb <657711802@qq.com>
2020-03-25 18:18:58 +0800

    添加启动页，适配刘海

--- Demo1/Demo1/Class/Tools/Common.swift
+++ Demo1/Demo1/Class/Tools/Common.swift
@@ -8,8 +8,26 @@
 
 import UIKit
 
-let kStatusBarH :CGFloat = 20
+let kStatusBarH :CGFloat = isNotchScreen == true ? 60 : 20
 let kNavigationBarH : CGFloat = 40
 let kTabBarH : CGFloat = 40
 let kScreenW = UIScreen.main.bounds.width
 let kScreenH = UIScreen.main.bounds.height
+// iPhoneX、iPhoneXR、iPhoneXs、iPhoneXs Max等
+// 判断刘海屏，返回true表示是刘海屏
+var isNotchScreen: Bool {
+        
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            return false
+        }
+        
+        let size = UIScreen.main.bounds.size
+        let notchValue: Int = Int(size.width/size.height * 100)
+        
+        if 216 == notchValue || 46 == notchValue {
+            
+            return true
+        }
+        
+        return false
+    }
